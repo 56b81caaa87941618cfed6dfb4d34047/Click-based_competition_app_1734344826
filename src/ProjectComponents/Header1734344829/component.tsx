1b853f76c64d0721c72cd14c1ecfd5bf67d338b0
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-16 18:59:47 +0100

    Update source code for component: Header1734344829

--- src/ProjectComponents/Header1734344829/component.tsx
+++ src/ProjectComponents/Header1734344829/component.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Header: React.FC = () => {
+const Hero: React.FC = () => {
+  
   return (
-    <header className="bg-blue-500 text-white p-4 w-full h-full"> {/* Full width and height */}
-      <div className="container mx-auto flex justify-between items-center h-full">
-        <div className="text-2xl font-bold">ClickWars</div>
+    <div className="bg-black w-full h-full relative">
+      <div className="absolute top-4 right-4">
+        <img src={`https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Click-based_competition_app_1734344826/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/389974a757ba43428bf44cb404b414a3.jpeg`} alt="Hero" className="w-16 h-16 rounded-full" />
       </div>
-    </header>
+    </div>
   );
 };
 
-export { Header as component };
+export { Hero as component }
+
